4891a4cb8ea9f2eb58fdb756054c39702e27a3f5
eoromes <prcd20020@gmail.com>
2025-09-06 12:46:13 -0300

    .

---
+++ app/data/collector.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+
+
+def get_stock_data(stocksTickers: list):
+    dailyData = []
+
+    for ticker in stocksTickers:
+        dat = yf.Ticker(ticker)
+
+        dailyData.append(dat.history(period="1d"))
+
+    return dailyData
